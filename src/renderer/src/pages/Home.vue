6de2e2b77047862ddd264b785c19f1d0732ee594
Akın Özgen <akinozgen17@outlook.com>
2022-01-13 15:07:50 +0300

    choices.js implemented

--- src/renderer/src/pages/Home.vue
+++ src/renderer/src/pages/Home.vue
@@ -1,31 +1,48 @@
 <template>
-    <section>
-      <h1 class="page-title">
-        Settings
-      </h1>
-      <div class="settings">
-        <label>
-          <div class="label-text">Start page</div>
-          <div class="input">
-            <select v-model="startPage" @change="changeStartPage">
-              <option v-for="(route, i) in routes" :selected="startPage == route.path" :value="route.path" :key="i">{{ route.name }}</option>
-            </select>
-          </div>
-        </label>
-      </div>
-    </section>
+  <section>
+    <h1 class="page-title">Settings</h1>
+    <div class="settings selectable">
+      <label>
+        <div class="label-text">Start page</div>
+        <div class="select">
+          <select
+            v-model="startPage"
+            @change="changeStartPage"
+            ref="selector"
+          ></select>
+        </div>
+      </label>
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
-  import { useRouter } from 'vue-router';
-  import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { computed, ref, onMounted } from 'vue';
+import Choices, { Choice } from 'choices.js';
+import 'choices.js/public/assets/styles/choices.min.css';
 
-  const router = useRouter();
-  const routes = computed(() => router.getRoutes());
-  const startPage = ref(localStorage.getItem('startPage') || '/');
+const selector = ref<HTMLSelectElement>();
+const router = useRouter();
+const routes = computed(() => router.getRoutes());
+const startPage = ref(localStorage.getItem('startPage') || '/');
 
-  function changeStartPage(e) {
-    startPage.value = e.target.value;
-    localStorage.setItem('startPage', startPage.value);
-  }
+function changeStartPage(e: any) {
+  startPage.value = e.target.value;
+  localStorage.setItem('startPage', startPage.value);
+}
+
+onMounted(() => {
+  new Choices(selector.value, {
+    choices: router.getRoutes().map<Choice>(
+      (route): Choice => ({
+        value: route.path,
+        label: route.name?.toString() || '',
+        selected: route.path === startPage.value
+      })
+    )
+  });
+
+  selector.value?.addEventListener('change', changeStartPage);
+});
 </script>
