faf360ec005d167938f90a8415a1f072da37a265
Akın Özgen <akinozgen17@outlook.com>
2022-01-20 21:30:46 +0300

    alarm page redesign

--- src/renderer/src/pages/Timer.vue
+++ src/renderer/src/pages/Timer.vue
@@ -1,120 +1,155 @@
 <template>
-  <div class="timer">
-    <div class="clock">
-      {{ currentTime }}
-    </div>
-    <div class="selects">
-      <div class="select">
-        <select multiple max="1" v-model="hours">
-          <option v-for="hour in numberRangeFilled(25)" :value="hour">{{ hour }}</option>
-        </select>
+  <section class="p-10">
+    <h1 class="text-left">Alarm</h1>
+    <div class="text-left pt-5 font-bold">
+      <div class="text-left">Current time: {{ currentTime }}</div>
+      <div class="pt-5 flex w-full justify-between align-baseline">
+        <div class="form-control flex-1 ml-2">
+          <label class="label">
+            <span class="label-text">Hour</span>
+          </label>
+          <select v-model="hours" ref="selector" class="select select-bordered w-full max-w-xs">
+            <option v-for="hour in numberRangeFilled(24)" :value="hour" :key="hour">
+              {{ hour }}
+            </option>
+          </select>
+        </div>
+        <div class="form-control flex-1 ml-2">
+          <label class="label">
+            <span class="label-text">Minute</span>
+          </label>
+          <select v-model="minutes" ref="selector" class="select select-bordered w-full max-w-xs">
+            <option v-for="minute in numberRangeFilled(60)" :value="minute" :key="minute">
+              {{ minute }}
+            </option>
+          </select>
+        </div>
+        <div class="form-control flex-1 ml-2">
+          <label class="label">
+            <span class="label-text">Second</span>
+          </label>
+          <select v-model="seconds" ref="selector" class="select select-bordered w-full max-w-xs">
+            <option v-for="second in numberRangeFilled(60)" :value="second" :key="second">
+              {{ second }}
+            </option>
+          </select>
+        </div>
       </div>
-      <div class="select">
-        <select multiple max="1" v-model="minutes">
-          <option v-for="minute in numberRangeFilled(61)" :value="minute">{{ minute }}</option>
-        </select>
+
+      <div class="pl-2 pt-5">
+        <button
+          @click="setAlarm()"
+          :class="isAlarmSet ? 'active btn-error' : 'btn-primary'"
+          class="btn btn-block"
+        >
+          {{ isAlarmSet ? 'Cancel Alarm' : 'Set Alarm' }}
+        </button>
       </div>
-      <div class="select">
-        <select multiple max="1" v-model="seconds">
-          <option v-for="second in numberRangeFilled(61)" :value="second">{{ second }}</option>
-        </select>
+
+      <h3 class="pl-2 pt-5">Select Alarm Tone</h3>
+      <div class="flex w-full pl-2 pt-2 gap-5">
+        <label class="btn btn-secondary btn-block btn-circle flex-1 pr-2">
+          <input
+            @click="tune = 'siren'"
+            type="radio"
+            name="tune"
+            :model="tune"
+            value="siren"
+            checked
+          />
+          <span class="pl-2">Siren</span>
+        </label>
+        <label class="btn btn-secondary btn-block btn-circle flex-1 pl-2">
+          <input @click="tune = 'nuke'" type="radio" name="tune" :model="tune" value="nuke" />
+          <span class="pl-2">Nuke</span>
+        </label>
       </div>
-    </div>
-    <div class="button">
-      <button @click="setAlarm()" :class="isAlarmSet ? 'active' : ''">
-        {{ isAlarmSet ? 'Cancel Alarm' : 'Set Alarm' }}
+
+      <button
+        @click="unsetAlarm()"
+        v-if="isAlarmSet"
+        :disabled="!isAlarmSet"
+        class="stop-alarm btn btn-error btn-block btn-circle flex-1 m-2 mt-10"
+      >
+        Stop Alarm
       </button>
+      <p class="notice text-center text-xl">{{ alarmNotice }}</p>
     </div>
-    <div class="radios">
-      <label>
-        <input @click="tune = 'siren'" type="radio" name="tune" :model="tune" value="siren" checked />
-        Siren
-      </label>
-      <label>
-        <input @click="tune = 'nuke'" type="radio" name="tune" :model="tune" value="nuke" />
-        Nuke
-      </label>
-    </div>
-    <div class="button" v-if="isAlarmSet">
-      <button @click="unsetAlarm()" :disabled="!isAlarmSet" class="stop-alarm">
-        Stop
-      </button>
-    </div>
-    <p class="notice">{{ alarmNotice }}</p>
-  </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
-  import { ref, Ref, onMounted } from 'vue';
+import { ref, Ref, onMounted } from 'vue';
 
-  import sirenMP3 from '../assets/siren.mp3';
-  import nukeMP3 from '../assets/nuke.mp3';
+import sirenMP3 from '../assets/siren.mp3';
+import nukeMP3 from '../assets/nuke.mp3';
 
-  const tune = ref('siren');
-  const currentDateTime = new Date();
-  const currentTime = ref(currentDateTime.toLocaleTimeString());
-  const alarmSetAt = ref("00:00:00");
-  const isAlarmSet = ref(false);
-  const alarmNotice = ref("");
-  const audioPlayer = ref(null);
+const tune = ref('siren');
+const currentDateTime = new Date();
+const currentTime = ref(currentDateTime.toLocaleTimeString());
+const alarmSetAt = ref('00:00:00');
+const isAlarmSet = ref(false);
+const alarmNotice = ref('');
+const audioPlayer = ref(null);
 
-  const hours = ref(0);
-  const minutes = ref(0);
-  const seconds = ref(0);
+const hours = ref('00');
+const minutes = ref('00');
+const seconds = ref('00');
 
-  setInterval(() => {
-    currentTime.value = (new Date()).toLocaleTimeString();
+setInterval(() => {
+  currentTime.value = new Date().toLocaleTimeString();
 
-    if (isAlarmSet.value !== true) return;
+  if (isAlarmSet.value !== true) return;
 
-    if (alarmSetAt.value == currentTime.value) {
-      playTune();
-    }
-  }, 1000);
-
-  function numberRangeFilled(length: Number):Array<String> {
-    return [...Array(length - 1).keys()].map(i => i.toString()).map(i => i.padStart(2, '0'));
+  if (alarmSetAt.value == currentTime.value) {
+    playTune();
   }
+}, 1000);
 
-  function setAlarm() {
-    if (isAlarmSet.value) {
-      return unsetAlarm();
-    }
-    alarmSetAt.value = `${hours.value}:${minutes.value}:${seconds.value}`;
-    isAlarmSet.value = true;
-    alarmNotice.value = `Alarm set for ${alarmSetAt.value}`;
-    console.log(tune);
+function numberRangeFilled(length: Number): Array<String> {
+  return [...Array(length).keys()].map((i) => i.toString()).map((i) => i.padStart(2, '0'));
+}
+
+function setAlarm() {
+  if (isAlarmSet.value) {
+    return unsetAlarm();
   }
+  alarmSetAt.value = `${hours.value}:${minutes.value}:${seconds.value}`;
+  isAlarmSet.value = true;
+  alarmNotice.value = `Alarm set for ${alarmSetAt.value}`;
+  console.log(tune);
+}
 
-  function unsetAlarm() {
-    alarmSetAt.value = "00:00:00";
-    isAlarmSet.value = false;
-    alarmNotice.value = "";
+function unsetAlarm() {
+  alarmSetAt.value = '00:00:00';
+  isAlarmSet.value = false;
+  alarmNotice.value = '';
 
-    if (audioPlayer.value) {
-      audioPlayer.value.pause();
-      audioPlayer.value = null;
-    }
+  if (audioPlayer.value) {
+    // @ts-ignore
+    audioPlayer.value.pause();
+    audioPlayer.value = null;
   }
+}
 
-  async function playTune() {
-    let tuneToPlay = null;
+async function playTune() {
+  let tuneToPlay = null;
 
-    switch (tune.value) {
-      case 'siren':
-        tuneToPlay = sirenMP3;
-        break;
-      case 'nuke':
-        tuneToPlay = nukeMP3;
-        break;
-    
-      default:
-        tuneToPlay = sirenMP3;
-        break;
-    }
-    
-    audioPlayer.value = new Audio(tuneToPlay);
-    audioPlayer.value.play();
+  switch (tune.value) {
+    case 'siren':
+      tuneToPlay = sirenMP3;
+      break;
+    case 'nuke':
+      tuneToPlay = nukeMP3;
+      break;
+
+    default:
+      tuneToPlay = sirenMP3;
+      break;
   }
-  
+  // @ts-ignore
+  audioPlayer.value = new Audio(tuneToPlay);
+  // @ts-ignore
+  audioPlayer.value.play();
+}
 </script>
