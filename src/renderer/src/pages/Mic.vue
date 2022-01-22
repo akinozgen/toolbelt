633c8308eb95befad68a9607c98da399611486ad
Akın Özgen <akinozgen17@outlook.com>
2022-01-22 19:27:36 +0300

    mic check page redesign

--- src/renderer/src/pages/Mic.vue
+++ src/renderer/src/pages/Mic.vue
@@ -1,56 +1,61 @@
 <template>
-    <div class="audio-player">
-        <button class="play-pause" @click="playPause()">
-            <img :src="currentIcon" alt="Play / Pause" tabindex="1">
-            <audio class="player" ref="player" />
-        </button>
-    </div>
+  <div class="flex justify-center align-middle h-full w-auto">
+    <button
+      class="btn self-center btn-primary btn-circle hover:scale-110 active:scale-100 scale-100 w-40 h-40 !transition !ease-linear animate-none"
+      @click="playPause()"
+    >
+      <img :src="currentIcon" alt="Play / Pause" tabindex="1" class="w-20" />
+      <audio class="player" ref="player" />
+    </button>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, Ref, ref } from "vue";
+import { onMounted, Ref, ref } from 'vue';
 
 import playIcon from '../assets/play.png';
 import pauseIcon from '../assets/pause.png';
 
-const player:Ref<HTMLAudioElement | null> = ref(null);
+const player: Ref<HTMLAudioElement | null> = ref(null);
 const currentIcon = ref(playIcon);
 const isPlaying = ref(false);
 
 function playPause() {
-    if (isPlaying.value) return pause();
-    play();
+  if (isPlaying.value) return pause();
+  play();
 }
 
 function play() {
-    isPlaying.value = true;
-    currentIcon.value = pauseIcon;
+  isPlaying.value = true;
+  currentIcon.value = pauseIcon;
 
-    player.value?.play();
+  player.value?.play();
 }
 
-function pause() {    
-    isPlaying.value = false;
-    currentIcon.value = playIcon;
+function pause() {
+  isPlaying.value = false;
+  currentIcon.value = playIcon;
 
-    player.value?.pause();
+  player.value?.pause();
 }
 
 function requestAudioPermission() {
-    navigator.mediaDevices.getUserMedia({
-        video: false,
-        audio: true
-    }).then((stream:MediaStream) => {
-        if (!player.value) return; 
-
-        player.value.srcObject = stream;
+  navigator.mediaDevices
+    .getUserMedia({
+      video: false,
+      audio: true
     })
-    .catch(e => console.log(e));
+    .then((stream: MediaStream) => {
+      if (!player.value) return;
+
+      player.value.srcObject = stream;
+    })
+    .catch((e) => console.log(e));
 }
 
 onMounted(() => {
-    if (!navigator.mediaDevices.getUserMedia) return alert('No camera found.');
+  if (!navigator.mediaDevices.getUserMedia) return alert('No camera found.');
 
-    requestAudioPermission();
+  requestAudioPermission();
 });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-    purge: ['./src/renderer/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-    },
-    variants: {
-      extend: {
-      },
-    },
-    plugins: [
-      require('daisyui'),
-      require('@tailwindcss/typography')
-    ],
-  }
+  purge: ['./src/renderer/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {
+    extend: {}
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/typography')]
+};
